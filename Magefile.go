267c53657c856830cbd0348b345c8d47a24045fd
Sergei Zyubin <sergei@choco.com>
2021-02-04 13:42:12 +0100

    Update Magefile to exclude unsupported Arm32 builds

--- Magefile.go
+++ Magefile.go
@@ -5,7 +5,13 @@ package main
 import (
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
+	"github.com/magefile/mage/mg"
 )
 
 // Default configures the default target.
-var Default = build.BuildAll
+func Build64() {
+	var arch64 = build.Build{}
+	mg.Deps(arch64.Linux, arch64.Windows, arch64.Darwin, arch64.LinuxARM64)
+}
+
+var Default = Build64
